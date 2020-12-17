ac07fbf1660d942cc8c658e60fcb4490e70186ab
codehaiku22 <mailstufftodeep@zoho.com>
2020-12-17 08:55:45 -0800

    Added more powerful restic scripts.

--- wsl-drvfs/restic_remove.sh
+++ wsl-drvfs/restic_remove.sh
@@ -1,12 +1,52 @@
 #!/bin/bash
-source ./mount_repo.sh
+
+#Input valid drive letter destinations.
+echo "Which drive should the snapshot be deleted from [u/e]?"
+read drive
+
+#If trimmed length of input is 0, exit.
+if [ $(echo ${#drive}) == 0 ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+#If input is not valid mounted driver letter, exit.
+if [ $drive != 'u' ] && [ $drive != 'e' ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+#If drive is not local, mount the drive.
+if [ $drive == 'u' ]
+then
+    source ./mount_repo.sh
+fi
+
+#Core restic commands
 export RESTIC_PASSWORD=$(cat /home/dgrewal/bin/restic/.restic_pwd)
-export RESTIC_REPOSITORY="/mnt/u/backups/restic/"
+export RESTIC_REPOSITORY="/mnt/$drive/backups/restic/"
+
 restic snapshots
+
+#Display disk usage for the restic repo.
 du -sh $RESTIC_REPOSITORY
+
+#Provide the option to remove any existing snapshots after backup is complete.
 echo "Please indicate the ID for the snapshot which is to be removed."
 read snapshot
+
+#If trimmed length of input is 0, exit.
+if [ $(echo ${#snapshot}) == 0 ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+#Delete the snapshot.
 restic forget $snapshot
 restic prune
 restic snapshots
+
 du -sh $RESTIC_REPOSITORY
--- wsl-drvfs/restic_restore.sh
+++ wsl-drvfs/restic_restore.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
-source ./mount_repo.sh
-export RESTIC_PASSWORD=$(cat  /home/dgrewal/bin/restic/.restic_pwd)
-export RESTIC_REPOSITORY="/mnt/u/backups/restic/"
-restore_point="/mnt/c/Users/dgrewal/Restores"
+
+#Input valid drive letter destinations.
+echo "Which drive should the restore occur from [u/e]?"
+read drive
+
+#If trimmed length of input is 0, exit.
+if [ $(echo ${#drive}) == 0 ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+#If input is not a valid mounted drive letter, exit.
+if [ $drive != 'u' ] && [ $drive != 'e' ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+#If drive is not local, mount the drive.
+if [ $drive == 'u' ]
+then
+    source ./mount_repo.sh
+fi
+
+#Core restic commands
+export RESTIC_PASSWORD=$(cat /home/dgrewal/bin/restic/.restic_pwd)
+export RESTIC_REPOSITORY="/mnt/$drive/backups/restic/"
+
+#Establish a location to restore the snapshot to.
+restorePoint="/mnt/c/Users/dgrewal/Restores"
+
+#Display disk usage for the restic repo.
 du -sh $RESTIC_REPOSITORY
+
 restic snapshots
-echo "Please indicate the ID of the snapshot which is to be restored to "$restore_point.
+
+#Request a snapshot Id to restore.
+echo "Please indicate the ID of the snapshot which is to be restored to "$restorePoint.
 read snapshot
-restic restore -v $snapshot --target /mnt/d/restores
+
+#If trimmed length of input is 0, exit.
+if [ $(echo ${#snapshot}) == 0 ]
+then
+    return 2>/dev/null
+    exit
+fi
+
+restic restore -v $snapshot --target $restorePoint
